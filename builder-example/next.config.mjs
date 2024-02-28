ee2936d05ac24a051656f58389318e2c4c24d2ee
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-02-28 13:19:20 -0700

    initial commit

---
+++ builder-example/next.config.mjs
@@ -0,0 +1,8 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = BuilderDevTools()({
+  reactStrictMode: true,
+});
+
+export default nextConfig;
